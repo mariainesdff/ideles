606149ec2c2a86bb76a3379e4691874d2b4fa463
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-08-09 10:31:43 +0100

    change P from term to type

--- src/finite_adeles_Q.lean
+++ src/finite_adeles_Q.lean
@@ -2,23 +2,24 @@ import data.nat.prime
 import data.set.finite
 import number_theory.padics
 
-def P := {p :  ℕ | fact (prime p)}
-variables [∀ p: P, fact (nat.prime p)]
---variables [Π p: P, has_one (ℚ_[p])]
+/-- The type of primes in ℕ -/
+def P := {p : ℕ // nat.prime p}
+
+instance (p: P) : fact (nat.prime p.1) := ⟨p.2⟩
 
 structure A_Q_f : Type :=
-(x : Π p : P, ℚ_[p])
+(x : Π p : P, ℚ_[p.1])
 (fin_supp : set.finite({ p : P | padic_norm_e (x p) > 1}))
 
 noncomputable instance inhabited_pre : inhabited A_Q_f :=
 begin
-  use λ p, (1: ℚ_[p]),
-  have h : {p : ↥P | @padic_norm_e p (_inst_1 p) 1 > 1} = ∅, {
+  use λ p, (1: ℚ_[p.1]),
+  have h : {p : P | padic_norm_e (1 : ℚ_[p.1]) > 1} = ∅, {
     rw set.eq_empty_iff_forall_not_mem,
     intros p hp,
     have h1: ¬ padic_norm_e 1 ≤  1, { exact hp.right},
-    have h2: @padic_norm_e p (_inst_1 p) 1 = 1, {
-      exact @padic_norm_e.one' p (_inst_1 p)
+    have h2: padic_norm_e (1 : ℚ_[p.1]) = 1, {
+      exact padic_norm_e.one'
     },
     exact h1 (le_of_eq h2),
   },
