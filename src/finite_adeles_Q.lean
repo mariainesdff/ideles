f9fe06ccbd2b27239401f1564fb63bdbfad24889
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2021-08-12 18:43:29 +0100

    Added  adeles as Q-algebra, adele_ring. Unified notation.

--- src/finite_adeles_Q.lean
+++ src/finite_adeles_Q.lean
@@ -1,10 +1,12 @@
 import data.nat.prime
 import data.set.finite
 import number_theory.padics
+import number_theory.divisors
 
+noncomputable theory
 open nat
 
-namespace fin_ad
+namespace fin_adeles
 
 instance (p : primes) : fact (nat.prime p) := ⟨p.2⟩
 
@@ -12,7 +14,9 @@ structure A_Q_f : Type :=
 (el : Π p : primes, ℚ_[p])
 (fin_supp : set.finite({ p : primes | padic_norm_e (el p) > 1}))
 
-noncomputable instance inhabited_pre : inhabited A_Q_f :=
+lemma eq_constr (a: A_Q_f): {A_Q_f . el := a.el, fin_supp := a.fin_supp} = a := by {cases a, refl}
+
+instance inhabited_pre : inhabited A_Q_f :=
 begin
   use λ p, (1: ℚ_[p]),
   have h : {p : primes | padic_norm_e (1 : ℚ_[p]) > 1} = ∅, {
@@ -24,44 +28,50 @@ begin
   exact set.finite_empty,
 end
 
-noncomputable def add : A_Q_f →  A_Q_f →  A_Q_f := begin
-  intros x y,
-  exact {el := (λ p :primes, (x.el p + y.el p)),
+section add_comm_group
+
+def add : A_Q_f →  A_Q_f →  A_Q_f := begin
+  intros a b,
+  exact {el := (λ p :primes, (a.el p + b.el p)),
    fin_supp := begin
-     let supp_sum := {p : primes | padic_norm_e (x.el p + y.el p) > 1},
-     let supp_x := {p : primes | padic_norm_e (x.el p) > 1},
-     let supp_y := {p : primes | padic_norm_e (y.el p) > 1},
-     have h_subset: supp_sum ⊆ (supp_x ∪ supp_y), 
+     let supp_sum := {p : primes | padic_norm_e (a.el p + b.el p) > 1},
+     let supp_a := {p : primes | padic_norm_e (a.el p) > 1},
+     let supp_b := {p : primes | padic_norm_e (b.el p) > 1},
+     have h_subset: supp_sum ⊆ (supp_a ∪ supp_b), 
      {intros p hp,
       rw set.mem_set_of_eq at hp,
       by_contradiction hnot,
       rw ← set.set_of_or at hnot,
       rw set.nmem_set_of_eq at hnot,
       push_neg at hnot,
-      cases hnot with hx hy,
-      have h_le_max, exact padic_norm_e.nonarchimedean' (x.el p) (y.el p),
+      cases hnot with ha hb,
+      have h_le_max, exact padic_norm_e.nonarchimedean' (a.el p) (b.el p),
       rw le_max_iff at h_le_max,
       cases h_le_max; 
       linarith,
      },
-     exact set.finite.subset (set.finite.union x.fin_supp y.fin_supp) h_subset,
+     exact set.finite.subset (set.finite.union a.fin_supp b.fin_supp) h_subset,
    end},
 end
 
-noncomputable instance : has_add A_Q_f := {add := fin_ad.add}
+instance : has_add A_Q_f := {add := fin_adeles.add}
 
-lemma add_apply (a b : A_Q_f) (p : primes) :
-  (a + b).el p = a.el p + b.el p := rfl
-
-theorem add_assoc: ∀ (a b c : A_Q_f), a + b + c = a + (b + c) := begin
-  intros a b c,
+theorem add_assoc (a b c : A_Q_f): a + b + c = a + (b + c) := begin
   unfold_projs,
   rw add,
   simp,
   exact add_assoc _ _ _,
 end
 
-noncomputable def zero : A_Q_f :=
+theorem add_comm (a b : A_Q_f): a + b = b + a := 
+begin
+  unfold_projs,
+  rw add,
+  simp,
+  exact add_comm _ _,
+end
+
+def zero : A_Q_f :=
 { el :=  λ p, (0: ℚ_[p]),
   fin_supp := 
 begin
@@ -75,65 +85,43 @@ begin
   exact set.finite_empty,
 end}
 
-noncomputable instance : has_zero A_Q_f := {zero := fin_ad.zero}
+instance : has_zero A_Q_f := {zero := fin_adeles.zero}
 
-variable a: A_Q_f
-
-
-lemma problem (a: A_Q_f): {A_Q_f . el := a.el, fin_supp := a.fin_supp} = a := begin
-  sorry,
-end
-
-theorem zero_add: ∀ (a : A_Q_f), 0 + a = a := 
+theorem zero_add (a : A_Q_f) : 0 + a = a := 
 begin
-  intro a,
   unfold_projs,
   rw zero,
   rw add,
   simp,
-  --change {A_Q_f . el := a.el, fin_supp := a.fin_supp} = a,
-  sorry,
+  exact eq_constr a,
 end 
 
-theorem add_zero: ∀ (a : A_Q_f), a + 0 = a := begin
-  intro a,
-  unfold_projs,
-  rw zero,
-  rw add,
-  simp,
-  sorry,
+theorem add_zero (a : A_Q_f): a + 0 = a := begin
+  rw add_comm,
+  exact zero_add a,
 end
--- next challenge : topological ring!
--- my suggestion:
--- (1) add_comm_group
--- (2) comm_ring
--- (3) topological_space
--- (4) topological_ring
 
--- {! !}
-
-noncomputable def neg : A_Q_f →  A_Q_f := begin
-  intro x,
-  exact {el := (λ p :primes, -(x.el p)),
+def neg : A_Q_f →  A_Q_f := begin
+  intro a,
+  exact {el := (λ p :primes, -(a.el p)),
    fin_supp := begin
-     have h: ∀ p: primes, padic_norm_e (-(x.el p)) = padic_norm_e (x.el p), {
+     have h: ∀ p: primes, padic_norm_e (-(a.el p)) = padic_norm_e (a.el p), {
        intro p,
-       exact padic_norm_e.neg (x.el p),
+       exact padic_norm_e.neg (a.el p),
       },
-     have h_eq: {p : primes | padic_norm_e (-(x.el p)) > 1} = {p : primes | padic_norm_e (x.el p) > 1}, 
+     have h_eq: {p : primes | padic_norm_e (-(a.el p)) > 1} = {p : primes | padic_norm_e (a.el p) > 1}, 
      {ext p,
      repeat {rw set.mem_set_of_eq},
      rwa (h p),
      },
      rw h_eq,
-     exact  x.fin_supp,
+     exact  a.fin_supp,
    end},
 end
 
-noncomputable instance : has_neg A_Q_f := {neg := fin_ad.neg}
+instance : has_neg A_Q_f := {neg := fin_adeles.neg}
 
-theorem add_left_neg : ∀ (a : A_Q_f), -a + a = 0 := begin
-  intro a,
+theorem add_left_neg (a : A_Q_f) : -a + a = 0 := begin
   unfold_projs,
   rw add,
   rw zero,
@@ -141,23 +129,18 @@ theorem add_left_neg : ∀ (a : A_Q_f), -a + a = 0 := begin
   exact add_left_neg _,
 end
 
-theorem add_comm : ∀ (a b : A_Q_f), a + b = b + a := 
-begin
-  intros a b,
-  unfold_projs,
-  rw add,
-  simp,
-  exact add_comm _ _,
-end
+end add_comm_group
 
-noncomputable def mul : A_Q_f →  A_Q_f →  A_Q_f := begin
-  intros x y,
-  exact {el := (λ p :primes, (x.el p * y.el p)),
+section comm_ring
+
+def mul : A_Q_f →  A_Q_f →  A_Q_f := begin
+  intros a b,
+  exact {el := (λ p :primes, (a.el p * b.el p)),
    fin_supp := begin
-     let supp_sum := {p : primes | padic_norm_e (x.el p * y.el p) > 1},
-     let supp_x := {p : primes | padic_norm_e (x.el p) > 1},
-     let supp_y := {p : primes | padic_norm_e (y.el p) > 1},
-     have h_subset: supp_sum ⊆ (supp_x ∪ supp_y), 
+     let supp_sum := {p : primes | padic_norm_e (a.el p * b.el p) > 1},
+     let supp_a := {p : primes | padic_norm_e (a.el p) > 1},
+     let supp_b := {p : primes | padic_norm_e (b.el p) > 1},
+     have h_subset: supp_sum ⊆ (supp_a ∪ supp_b), 
      {intros p hp,
       rw set.mem_set_of_eq at hp,
       rw padic_norm_e.mul' _ _  at hp,
@@ -165,26 +148,32 @@ noncomputable def mul : A_Q_f →  A_Q_f →  A_Q_f := begin
       rw ← set.set_of_or at hnot,
       rw set.nmem_set_of_eq at hnot,
       push_neg at hnot,
-      cases hnot with hx hy,
-      have h_le: padic_norm_e (x.el p) * padic_norm_e (y.el p) ≤ 1 * 1 := mul_le_mul hx hy (padic_norm_e.nonneg (y.el p)) (by linarith),
+      cases hnot with ha hb,
+      have h_le: padic_norm_e (a.el p) * padic_norm_e (b.el p) ≤ 1 * 1 := mul_le_mul ha hb (padic_norm_e.nonneg (b.el p)) (by linarith),
       rw mul_one at h_le,
       linarith, 
      },
-     exact set.finite.subset (set.finite.union x.fin_supp y.fin_supp) h_subset,
+     exact set.finite.subset (set.finite.union a.fin_supp b.fin_supp) h_subset,
    end},
 end
 
-noncomputable instance : has_mul A_Q_f := {mul := fin_ad.mul}
+instance : has_mul A_Q_f := {mul := fin_adeles.mul}
 
-theorem mul_assoc : ∀ (a b c : A_Q_f), a * b * c= a * (b * c):= begin
-  intros a b c,
+theorem mul_assoc (a b c : A_Q_f) : a * b * c = a * (b * c):= begin
   unfold_projs,
   rw mul,
   simp,
   exact mul_assoc _ _ _,
 end
 
-noncomputable def one : A_Q_f :=
+theorem mul_comm (a b : A_Q_f) : a * b = b * a := begin
+  unfold_projs,
+  rw mul,
+  simp,
+  exact mul_comm _ _,
+end
+
+def one : A_Q_f :=
 { el := λ p, (1: ℚ_[p]),
   fin_supp := begin
       have h : {p : primes | padic_norm_e (1 : ℚ_[p]) > 1} = ∅, {
@@ -197,36 +186,22 @@ noncomputable def one : A_Q_f :=
     end
 }
 
-noncomputable instance : has_one A_Q_f := {one := fin_ad.one}
+instance : has_one A_Q_f := {one := fin_adeles.one}
 
-theorem one_mul: ∀ (a : A_Q_f), 1 * a = a := begin
-  intro a,
+theorem one_mul (a : A_Q_f): 1 * a = a := begin
   unfold_projs,
   rw mul,
   rw one,
   simp,
-  sorry,
+  exact eq_constr a,
 end
 
-theorem mul_one: ∀ (a : A_Q_f), a * 1 = a := begin
-  intro a,
-  unfold_projs,
-  rw mul,
-  rw one,
-  simp,
-  sorry,
+theorem mul_one (a : A_Q_f): a * 1 = a := begin
+  rw mul_comm,
+  exact one_mul a,
 end
 
-theorem mul_comm : ∀ (a b : A_Q_f), a * b = b * a := begin
-  intros a b,
-  unfold_projs,
-  rw mul,
-  simp,
-  exact mul_comm _ _,
-end
-
-theorem left_distrib: ∀ (a b c : A_Q_f), a * (b + c) = a * b + a * c := begin
-  intros a b c,
+theorem left_distrib (a b c : A_Q_f):  a * (b + c) = a * b + a * c := begin
   unfold_projs,
   rw mul,
   rw add,
@@ -234,49 +209,172 @@ theorem left_distrib: ∀ (a b c : A_Q_f), a * (b + c) = a * b + a * c := begin
   exact left_distrib _ _ _,
 end
 
-theorem right_distrib: ∀ (a b c : A_Q_f), (a + b) * c = a * c + b * c := begin
-  intros a b c,
-  unfold_projs,
-  rw mul,
-  rw add,
-  simp,
-  exact right_distrib _ _ _,
+theorem right_distrib (a b c : A_Q_f) : (a + b) * c = a * c + b * c := begin
+  repeat {rw mul_comm _ c},
+  exact left_distrib _ _ _,
 end
 
-end fin_ad
+end comm_ring
+
+/- section topological_space
+
+def is_open : set A_Q_f → Prop := sorry
+
+theorem is_open_univ : is_open set.univ := sorry
+
+theorem is_open_inter (S T : set A_Q_f) : is_open S → is_open T → is_open (S ∩ T) := sorry
+
+theorem is_open_sUnion (S : set (set A_Q_f)) : (∀ (T : set A_Q_f), T ∈ S → is_open T) → is_open (⋃₀S) := sorry
 
 
-open fin_ad
+end topological_space -/
 
-noncomputable instance : add_comm_group A_Q_f :=
-{ add := fin_ad.add,
-  add_assoc := fin_ad.add_assoc,
-  zero := fin_ad.zero,
-  zero_add := fin_ad.zero_add,
-  add_zero := fin_ad.add_zero,
-  neg := fin_ad.neg,
-  add_left_neg := fin_ad.add_left_neg,
-  add_comm := fin_ad.add_comm
+
+instance : add_comm_group A_Q_f :=
+{ add := fin_adeles.add,
+  add_assoc := fin_adeles.add_assoc,
+  zero := fin_adeles.zero,
+  zero_add := fin_adeles.zero_add,
+  add_zero := fin_adeles.add_zero,
+  neg := fin_adeles.neg,
+  add_left_neg := fin_adeles.add_left_neg,
+  add_comm := fin_adeles.add_comm
 }
 
-noncomputable instance : comm_ring A_Q_f  := 
+instance : comm_ring A_Q_f  := 
 { 
-  mul := fin_ad.mul,
-  mul_assoc := fin_ad.mul_assoc,
-  one := fin_ad.one,
-  one_mul := fin_ad.one_mul,
-  mul_one := fin_ad.mul_one,
-  left_distrib := fin_ad.left_distrib,
-  right_distrib := fin_ad.right_distrib,
-  mul_comm := fin_ad.mul_comm,
+  mul := fin_adeles.mul,
+  mul_assoc := fin_adeles.mul_assoc,
+  one := fin_adeles.one,
+  one_mul := fin_adeles.one_mul,
+  mul_one := fin_adeles.mul_one,
+  left_distrib := fin_adeles.left_distrib,
+  right_distrib := fin_adeles.right_distrib,
+  mul_comm := fin_adeles.mul_comm,
   
   -- TODO: figure out how not to repeat this
-  add := fin_ad.add,
-  add_assoc := fin_ad.add_assoc,
-  zero := fin_ad.zero,
-  zero_add := fin_ad.zero_add,
-  add_zero := fin_ad.add_zero,
-  neg := fin_ad.neg,
-  add_left_neg := fin_ad.add_left_neg,
-  add_comm := fin_ad.add_comm,
+  add := fin_adeles.add,
+  add_assoc := fin_adeles.add_assoc,
+  zero := fin_adeles.zero,
+  zero_add := fin_adeles.zero_add,
+  add_zero := fin_adeles.add_zero,
+  neg := fin_adeles.neg,
+  add_left_neg := fin_adeles.add_left_neg,
+  add_comm := fin_adeles.add_comm,
 }
+section Q_algebra
+
+def map_Q_fin_adeles : ℚ → A_Q_f := (λ r, {A_Q_f . el := (λ p, (r: ℚ_[p])), fin_supp := begin
+  let supp := {p : primes | padic_norm_e ↑r > 1},
+  let im_supp  := (λ p: primes, p.val) '' supp,
+
+  have h1 : im_supp ⊆ divisors r.denom,
+  { intros p hp,
+    rw ← finset.set_of_mem,
+    rw set.mem_set_of_eq,
+    rcases hp with ⟨p, hp, rfl⟩,
+    rw set.mem_set_of_eq at hp,
+    rcases hp with ⟨-, hp⟩,
+    rw nat.mem_divisors,
+    split,
+    {simp,
+     by_contradiction hr,
+     have h := padic_norm_e.norm_rat_le_one hr,
+     rw ← padic_norm_e.is_norm at h,
+     rw ← rat.cast_one at h,
+     rw rat.cast_le at h,
+     exact hp h,
+    },
+    exact rat.denom_ne_zero r,
+  },
+  exact set.finite_of_finite_image (set.inj_on_of_injective (primes.coe_nat_inj) supp) (set.finite.subset (set.finite_mem_finset (divisors r.denom)) h1),
+end
+})
+
+theorem map_one' : map_Q_fin_adeles 1 = 1 := begin
+  unfold map_Q_fin_adeles,
+  rw ← eq_constr 1,
+  simp,
+  refl,
+end
+
+theorem map_mul' (r s : ℚ) : map_Q_fin_adeles (r * s) = map_Q_fin_adeles r * map_Q_fin_adeles s := begin
+  unfold map_Q_fin_adeles,
+  unfold_projs,
+  unfold mul,
+  simp,
+  ext p,
+  exact padic.coe_mul p,
+end
+
+theorem map_zero' : map_Q_fin_adeles 0 = 0 := begin
+  unfold map_Q_fin_adeles,
+  rw ← eq_constr 0,
+  simp,
+  refl,
+end
+
+theorem map_add' (r s : ℚ) : map_Q_fin_adeles (r + s) = map_Q_fin_adeles r + map_Q_fin_adeles s := begin
+  unfold map_Q_fin_adeles,
+  unfold_projs,
+  unfold add,
+  simp,
+  ext p,
+  exact padic.coe_add p,
+end
+
+def smul : ℚ → A_Q_f → A_Q_f := λ (r : ℚ) (a: A_Q_f), mul (map_Q_fin_adeles r) a
+
+instance : has_scalar ℚ A_Q_f := {smul := fin_adeles.smul}
+
+theorem smul_def' (r : ℚ) (a : A_Q_f) : r • a = map_Q_fin_adeles r * a := begin
+  unfold_projs,
+  unfold smul,
+end
+
+theorem fin_adeles.commutes' (r : ℚ) (a : A_Q_f):  map_Q_fin_adeles r * a = a * map_Q_fin_adeles r := mul_comm _ _
+
+def hom_Q_fin_adeles : ring_hom ℚ A_Q_f := { to_fun := fin_adeles.map_Q_fin_adeles,
+  map_one' := fin_adeles.map_one',
+  map_mul' := fin_adeles.map_mul',
+  map_zero' := fin_adeles.map_zero',
+  map_add' := fin_adeles.map_add' }
+
+instance : algebra ℚ A_Q_f := { smul := fin_adeles.smul,
+  to_fun := fin_adeles.map_Q_fin_adeles,
+  -- TODO: remove
+  map_one' := fin_adeles.map_one',
+  map_mul' := fin_adeles.map_mul',
+  map_zero' := fin_adeles.map_zero',
+  map_add' := fin_adeles.map_add',
+  commutes' := fin_adeles.commutes',
+  smul_def' := fin_adeles.smul_def' }
+
+end Q_algebra
+/- instance : topological_space A_Q_f := { 
+  is_open := fin_adeles.is_open,
+  is_open_univ := fin_adeles.is_open_univ,
+  is_open_inter := fin_adeles.is_open_inter,
+  is_open_sUnion := fin_adeles.is_open_sUnion }
+
+instance : topological_ring A_Q_f := { 
+  continuous_add := sorry,
+  continuous_mul := sorry,
+  continuous_neg := sorry} -/
+
+end fin_adeles
+
+namespace adeles
+open fin_adeles
+
+def A_Q := A_Q_f × ℝ
+instance : semiring A_Q := prod.semiring
+instance : algebra ℚ A_Q := algebra.prod.algebra ℚ A_Q_f ℝ 
+
+open algebra
+
+def finite_adele_ring (K : Type)[field K][algebra ℚ K] := tensor_product ℚ K A_Q_f
+
+def adele_ring (K : Type)[field K][algebra ℚ K] := tensor_product ℚ K A_Q
+
+end adeles
