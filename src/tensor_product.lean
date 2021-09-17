d8061a82e34596033b19244e0f45a67997d20a44
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2021-09-17 12:57:50 +0100

    Tensor product

--- src/tensor_product.lean
+++ src/tensor_product.lean
@@ -1,24 +1,10 @@
 import ring_theory.tensor_product
 import ring_theory.localization
 
-variables {R : Type*} [comm_ring R] {S : submonoid R} (A : Type*) [comm_ring A] [algebra R A] (B : Type*) [comm_ring B] [algebra R B] [is_localization S B]
+variables {R : Type*} [comm_ring R] {S : submonoid R} (A : Type*) [comm_ring A] [algebra R A] 
+(B : Type*) [comm_ring B] [algebra R B] [is_localization S B]
 
 open_locale tensor_product
-
-/- def image_submonoid : submonoid A :=
-{ carrier  := (algebra_map R A)'' S,
-  one_mem' := by {rw set.mem_image, use [1, S.one_mem'], simp},
-  mul_mem' := 
-  begin
-    intros a b ha hb,
-    rw set.mem_image at ha hb ⊢,
-    rcases ha with ⟨r, hr, hr1⟩ ,
-    rcases hb with ⟨s, hs, hs1⟩,
-    use [r*s, S.mul_mem' hr hs],
-    rw ← hr1,
-    rw ← hs1,
-    exact (algebra_map R A).map_mul r s,
-  end } -/
 
 instance : algebra A (B ⊗[R] A)  := { smul := begin
   intros a x,
@@ -35,7 +21,8 @@ instance : algebra A (B ⊗[R] A)  := { smul := begin
 
 /- lemma tensor_product_with_localization (B : Type*) [comm_ring B] [algebra R B] [is_localization S B] : is_localization (algebra.algebra_map_submonoid A S) (B ⊗[R] A) -/
 
-lemma tensor_product_with_localization (B : Type*) [comm_ring B] [algebra R B] [is_localization S B] : is_localization (submonoid.map (algebra_map R A : R →* A) S) (B ⊗[R] A) := 
+lemma tensor_product_with_localization (B : Type*) [comm_ring B] [algebra R B] 
+  [is_localization S B] : is_localization (submonoid.map (algebra_map R A : R →* A) S) (B ⊗[R] A) := 
 { map_units := 
   begin
     rintro ⟨y', hy'⟩,
