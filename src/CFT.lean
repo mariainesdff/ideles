888c671002cba170a5504f48b541f1daa6b86726
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2022-02-01 15:59:50 +0000

    lint CFT

--- src/CFT.lean
+++ src/CFT.lean
@@ -6,13 +6,26 @@ Authors: María Inés de Frutos-Fernández
 import ideles_K
 import galois
 
+/-!
+# Main Theorem of Global Class Field Theory
+We state the main theorem of global class field theory for number fields. 
+Given a number field `K`, denote by `G_K_ab` the topological abelianization of its absolute Galois
+group. The main theorem of global class field theory states that `G_K_ab` is isomorphic to the 
+quotient `C_K_1` of the idèle class group of `K` by its identity component, as topological groups. 
+-/
+
+noncomputable theory
+
 variables (K : Type) [field K] [number_field K]
 
-theorem main_theorem_of_global_CFT.group_isomorphism :
+/-- The first part of the theorem is the claim that `G_K_ab` is isomorphic to `C_K_1` as groups.-/
+def main_theorem_of_global_CFT.group_isomorphism :
   (number_field.C_K K) ⧸ (subgroup.connected_component_of_one (number_field.C_K K)) ≃* (G_K_ab K) :=
-sorry
+force_noncomputable sorry
 
-noncomputable theorem main_theorem_of_global_CFT.homeomorph :
+/-- The second part claims that the above isomorphism of abstract groups is also a homeomorphism,
+and hence it is an isomorphism of topological groups. -/
+def main_theorem_of_global_CFT.homeomorph :
   homeomorph ((number_field.C_K K) ⧸ (subgroup.connected_component_of_one (number_field.C_K K)))
     (G_K_ab K) := 
 { continuous_to_fun  := sorry,
